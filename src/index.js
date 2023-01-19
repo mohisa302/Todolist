0ec568977afcf71806648f82dbf8690688026e15
mohi <mohisadeghi302@gmail.com>
2023-01-19 05:02:36 +0330

    Add list form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './style.css';
+
+const list = document.querySelector('.list-container');
+const listData = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'complete to Do list',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'read for exam',
+    completed: false,
+    index: 0,
+  },
+];
+
+listData.forEach((task, listIndex) => {
+  const { description, completed, index } = task;
+  list.innerHTML += `
+  <li class="task">
+  <input type="checkbox" id="${index}" name="task"/><del>${description}</del>
+  </li>
+  <hr class="list-line"/>
+  `;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -18,4 +18,5 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
   },
+  mode: 'production',
 };
