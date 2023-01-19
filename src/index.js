edc13e03e587d13f271d2fbb60d9905302116dd9
mohi <mohisadeghi302@gmail.com>
2023-01-19 11:30:28 +0330

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -29,6 +29,5 @@ listData.forEach((task, listIndex) => {
   </li>
   <hr class="list-line"/>
   `;
-  if (listIndex === listData.length - 1)
-    form.innerHTML += `<button class="complete-btn">Clear all completed</button>`;
+  if (listIndex === listData.length - 1) form.innerHTML += '<button class="complete-btn">Clear all completed</button>';
 });
