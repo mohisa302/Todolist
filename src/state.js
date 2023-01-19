5bd7b7a9a295c830bfa577af0fa3e969bb45e944
mohi <mohisadeghi302@gmail.com>
2023-01-20 02:37:36 +0330

    Revert "Fix linters error"
    This reverts commit 6062d108af24021c27bd68ef0a02960d4d74a33f.

--- src/state.js
+++ src/state.js
@@ -1,15 +1,14 @@
 import display from './ui.js';
-
 const clearCom = document.querySelector('.complete-btn');
 
 clearCom.addEventListener('click', () => {
-  let listData = JSON.parse(localStorage.getItem('tasks')) || [];
-  if (listData) {
-    listData = listData.filter((task) => !task.completed);
-  }
-  listData.forEach((taskDay, indexDay) => {
-    taskDay.index = indexDay;
+    let listData = JSON.parse(localStorage.getItem('tasks')) || [];
+    if (listData) {
+      listData = listData.filter((task) => !task.completed);
+    }
+    listData.forEach((taskDay, indexDay) => {
+      taskDay.index = indexDay;
+    });
+    localStorage.setItem('tasks', JSON.stringify(listData));
+    display();
   });
-  localStorage.setItem('tasks', JSON.stringify(listData));
-  display();
-});
