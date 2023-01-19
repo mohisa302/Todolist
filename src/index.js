8012d7717273e99c6b3249257c5c2417101b53b1
mohi <mohisadeghi302@gmail.com>
2023-01-19 20:33:38 +0330

    Split functionality

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import './style.css';
+import { display } from './ui.js';
+import './user';
 import { listData } from './tasks';
 
-const list = document.querySelector('.list-container');
-const form = document.querySelector('.form-container');
-
-listData.forEach((task, listIndex) => {
-  const { description, index } = task;
-  list.innerHTML += `
-  <li class="task">
-  <input type="checkbox" id="${index}" class="check-box" name="task"/><del>${description}</del>
-  </li>
-  <hr class="list-line"/>
-  `;
-  if (listIndex === listData.length - 1)
-    form.innerHTML +=
-      '<button class="complete-btn">Clear all completed</button>';
+window.addEventListener('load', () => {
+  listData = JSON.parse(localStorage.getItem('tasks'));
+  display();
 });
+
+document.addEventListener('DOMContentLoaded', display);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line import/no-unresolved
+const path = require('path');
 
 module.exports = {
   module: {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+    },
     rules: [
       {
         test: /\.css$/,
@@ -12,6 +18,10 @@ module.exports = {
   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
   devServer: {
     historyApiFallback: true,
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   mode: 'production',
 };
