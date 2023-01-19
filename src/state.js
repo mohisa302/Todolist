6062d108af24021c27bd68ef0a02960d4d74a33f
mohi <mohisadeghi302@gmail.com>
2023-01-20 02:25:20 +0330

    Fix linters error

--- src/state.js
+++ src/state.js
@@ -1,14 +1,15 @@
 import display from './ui.js';
+
 const clearCom = document.querySelector('.complete-btn');
 
 clearCom.addEventListener('click', () => {
-    let listData = JSON.parse(localStorage.getItem('tasks')) || [];
-    if (listData) {
-      listData = listData.filter((task) => !task.completed);
-    }
-    listData.forEach((taskDay, indexDay) => {
-      taskDay.index = indexDay;
-    });
-    localStorage.setItem('tasks', JSON.stringify(listData));
-    display();
+  let listData = JSON.parse(localStorage.getItem('tasks')) || [];
+  if (listData) {
+    listData = listData.filter((task) => !task.completed);
+  }
+  listData.forEach((taskDay, indexDay) => {
+    taskDay.index = indexDay;
   });
+  localStorage.setItem('tasks', JSON.stringify(listData));
+  display();
+});
